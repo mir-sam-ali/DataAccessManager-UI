16d518ccb1e789187b5f92363e88004aafc4bfe1
Mir Sameed Ali <meersameed@gmail.com>
2021-07-01 07:00:13 +0530

    BrowserRouter changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import AdminConfig from './Components/AdminConfig/AdminConfig';
-import DataAccess from './Components/DataAccess/DataAccess';
+// import DataAccess from './Components/DataAccess/DataAccess';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter basename="/">
+    <HashRouter basename="/">
       <div className="App">
         <Switch>
-          <Route path={'/'} exact component={DataAccess} />
-          <Route path={'/config'} exact component={AdminConfig} />
+          {/* <Route path={'/'} exact component={DataAccess} /> */}
+          <Route path={['/', 'config']} component={AdminConfig} />
         </Switch>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
